69991c3d7aad71115a05c7d1c21dc7c7ee7d842e
Brandon <cohbrandon@gmail.com>
2022-02-18 18:07:29 -0500

    added picture of me

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import vrImg from "../../assets/vr.png";
+import meImg from "../../assets/me.jpg";
 import Link from "next/link";
 
 const About: React.FC = () => {
@@ -12,14 +13,21 @@ const About: React.FC = () => {
           <div className="container flex flex-col items-center justify-center min-h-screen py-12 mx-auto sm:py-24">
             <div className="flex-col items-start justify-center w-11/12 mb-48 sm:w-2/3 lg:flex sm:mb-12">
               <div className="flex flex-row">
-                <h1 className="mt-3 text-4xl font-black leading-10 text-center text-white cursor-default sm:text-2xl md:text-4xl lg:text-7xl xl:text-8xl md:mt-0 md:leading-10">
-                  Hi, I'm Brandon!
-                </h1>
-                <div className="relative w-16 h-16 ml-2 sm:w-24 sm:h-24">
-                  <Image src={vrImg} alt="vrImage" layout="fill" />
+                <div>
+                  <h1 className="mt-3 text-4xl font-black leading-10 text-center text-white cursor-default sm:text-2xl md:text-4xl lg:text-7xl xl:text-8xl md:mt-1 md:leading-10">
+                    Hi, I'm Brandon!
+                  </h1>
+                </div>
+                <div className="relative ml-4 w-14 h-14 sm:w-28 sm:h-28">
+                  <Image
+                    src={meImg}
+                    alt="meImage"
+                    layout="fill"
+                    className="object-cover my-4 rounded-full shadow-sm shadow-black"
+                  />
                 </div>
               </div>
-              <p className="mt-2 text-3xl font-black leading-7 text-teal-600 cursor-default sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl md:leading-10 md:mt-4">
+              <p className="text-3xl font-black leading-7 text-teal-600 cursor-default sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl md:leading-10">
                 I am a Software Engineer who lives and works in South Florida.
                 Coding is my passion.{" "}
               </p>
