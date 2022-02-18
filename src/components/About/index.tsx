e289491e0342627a7aa6e8626b1291ccf60228b6
Brandon <cohbrandon@gmail.com>
2022-02-18 16:14:40 -0500

    added homepage contact button

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import vrImg from "../../assets/vr.png";
+import Link from "next/link";
 
 const About: React.FC = () => {
   return (
@@ -25,6 +26,31 @@ const About: React.FC = () => {
               {/* <p className="mt-6 text-3xl font-bold leading-7 text-teal-600 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl md:leading-10 md:mt-8">
                 Check out the links at the top to learn more about what I do!
               </p> */}
+              <Link href="/contact" passHref>
+                <button className="px-6 py-4 mt-8 text-lg leading-none text-white bg-teal-600 rounded-md shadow-md cursor-pointer sm:py-5 sm:mt-10 sm:text-4xl focus:outline-none shadow-black">
+                  {/* <svg
+                  className="mr-2 w-7 h-7"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                  ></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  ></path>
+                </svg> */}
+                  Contact me!
+                </button>
+              </Link>
             </div>
           </div>
         </div>
