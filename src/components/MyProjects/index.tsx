922f4f88bd10c3b7ae289ffb9d06e17c57e41d7e
Brandon <cohbrandon@gmail.com>
2022-02-23 18:42:11 -0500

    added graceshopper as a project and correct links to github

--- src/components/MyProjects/index.tsx
+++ src/components/MyProjects/index.tsx
@@ -63,6 +63,51 @@ const MyProjects: React.FC = () => {
                 </div>
               </div>
               <div className="rounded-md">
+                <div className="flex flex-col justify-between w-full h-full px-4 py-5 mb-6 transition duration-300 ease-in-out delay-150 border rounded-lg shadow-lg bg-amber-500 border-amber-500 shadow-black hover:-translate-y-1 hover:scale-105 hover:bg-amber-500">
+                  <div>
+                    <a
+                      href="https://github.com/Prime-Directive-Detectives/GraceShopper"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-white hover:text-white"
+                    >
+                      <h4 className="mb-3 text-3xl font-bold text-gray-100">
+                        Graceshopper
+                      </h4>
+                    </a>
+                    <p className="text-gray-100 cursor-default sm:text-lg">
+                      An ecommerce app built and designed in a week to showcase
+                      my coding skills.
+                    </p>
+                    <p className="mt-2 text-gray-100 cursor-default sm:text-lg">
+                      Tech stack: JavascriptScript, React/NextJS, TailwindCSS,
+                      Swiper.js, Stripe, Heroku
+                    </p>
+                  </div>
+                  <div>
+                    <div className="flex items-center justify-between text-gray-100">
+                      <p className="cursor-default text-md">January 19, 2022</p>
+                      <div className="flex items-center justify-center w-8 h-8 text-white bg-gray-100 rounded-full">
+                        <a
+                          href="https://github.com/Prime-Directive-Detectives/GraceShopper"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                          </svg>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="rounded-md">
                 <div className="flex flex-col justify-between w-full h-full px-4 py-5 mb-6 transition duration-300 ease-in-out delay-150 border rounded-lg shadow-lg shadow-black bg-cyan-500 border-cyan-500 hover:-translate-y-1 hover:scale-105 hover:bg-cyan-500">
                   <div>
                     <a
@@ -156,7 +201,7 @@ const MyProjects: React.FC = () => {
                 <div className="flex flex-col justify-between w-full h-full px-4 py-5 mb-6 transition duration-300 ease-in-out delay-150 bg-blue-500 border border-blue-500 rounded-lg shadow-lg shadow-black hover:-translate-y-1 hover:scale-105 hover:bg-blue-500">
                   <div>
                     <a
-                      href="https://github.com/OwsleyJr/Home-UI"
+                      href="https://github.com/OwsleyJr/portfolio"
                       target="_blank"
                       rel="noreferrer"
                       className="text-white hover:text-white"
@@ -181,7 +226,7 @@ const MyProjects: React.FC = () => {
                       </p>
                       <div className="flex items-center justify-center w-8 h-8 text-white bg-gray-100 rounded-full">
                         <a
-                          href="https://github.com/OwsleyJr/Home-UI"
+                          href="https://github.com/OwsleyJr/portfolio"
                           target="_blank"
                           rel="noreferrer"
                         >
