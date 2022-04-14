f77a34991dc3535e7cd399dbef0bf0a3064b9c67
Brandon <cohbrandon@gmail.com>
2022-04-14 11:42:31 -0400

    moved home page text up a little on mobile

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const About: React.FC = () => {
   return (
-    <div className="pt-40">
+    <div className="pt-32 sm:pt-40">
       <div className="container flex flex-col items-center justify-center min-h-full py-12 mx-auto sm:py-24">
         <div className="flex-col items-start justify-center w-11/12 mb-48 sm:w-2/3 sm:flex sm:mb-12">
           <div className="flex flex-row">
