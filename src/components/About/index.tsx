7f00fac711e48f2b640788e261687f76e410dfbf
Brandon <cohbrandon@gmail.com>
2022-10-11 06:22:26 -0700

    changed leading size of about page

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 
 const About: React.FC = () => {
   return (
-    <div className="flex flex-col items-start justify-center pt-16 mx-auto w-11/12 sm:w-2/3 sm:pt-24">
+    <div className="flex flex-col justify-center pt-16 mx-auto w-11/12 sm:w-2/3 sm:pt-24">
       <div className="bg-[#11212c]">
         <h1 className="mt-1 text-4xl font-black items-start flex leading-10 text-center text-white cursor-default sm:text-4xl md:text-5xl lg:text-7xl xl:text-7xl 2xl:text-8xl sm:leading-11">
           Hi, I'm Brandon!
         </h1>
         <div>
-          <p className="mt-2 text-3xl font-black leading-7 text-cyan-500 cursor-default sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-5xl md:leading-10">
+          <p className="mt-2 text-3xl font-black leading-8 text-cyan-500 cursor-default sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-5xl md:leading-10">
             I am a Software Engineer who lives and works in South Florida.
           </p>
         </div>
 
         <div>
-          <p className="mt-3 sm:mt-2 text-xl font-black leading-7 text-cyan-500 cursor-default sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-4xl md:leading-10">
+          <p className="mt-3 sm:mt-2 text-xl font-black leading-6 text-cyan-500 cursor-default sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-4xl md:leading-10">
             I work on an open-source project called{" "}
             <Link href="https://github.com/sct/overseerr">
               <a className="text-white hover:text-white">Overseerr</a>
