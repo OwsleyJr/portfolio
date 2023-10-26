2e445ba9dd6cf7acf9d3c88757c3b338de39cfe5
Brandon Cohen <brandon@z3hn.dev>
2023-10-26 17:50:00 -0400

    working framer motion exit animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+"use client";
+import { useRef, useContext } from "react";
+import { LayoutRouterContext } from "next/dist/shared/lib/app-router-context";
 import { Inter } from "next/font/google";
 import Navbar from "../components/layout/navbar";
-import LayoutTransition from "../components/layoutTransition";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 import "../styles/globals.css";
 
 const inter = Inter({
@@ -8,11 +12,30 @@ const inter = Inter({
   display: "swap",
 });
 
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
+};
+
+const FrozenRouter = ({ children }: { children: React.ReactNode }) => {
+  const context = useContext(LayoutRouterContext);
+  const frozen = useRef(context).current;
+
+  return (
+    <LayoutRouterContext.Provider value={frozen}>
+      {children}
+    </LayoutRouterContext.Provider>
+  );
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
   return (
     <html lang="en" className={inter.className}>
       <head>
@@ -24,7 +47,21 @@ export default function RootLayout({
       <body>
         <div className="flex flex-col min-h-screen min-w-0">
           <Navbar />
-          <LayoutTransition>{children}</LayoutTransition>
+          <AnimatePresence
+            mode="wait"
+            onExitComplete={() => window.scrollTo(0, 0)}
+          >
+            <motion.div
+              key={pathname}
+              initial="hidden"
+              animate="enter"
+              exit="exit"
+              variants={variants}
+              transition={{ type: "linear" }}
+            >
+              <FrozenRouter>{children}</FrozenRouter>
+            </motion.div>
+          </AnimatePresence>
         </div>
       </body>
     </html>
