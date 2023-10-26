ff88eb776b2650eb031f2396687a9b30dc011ad9
Brandon Cohen <brandon@z3hn.dev>
2023-10-26 18:15:21 -0400

    skills updated

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import Skills from "../../components/skills";
+import Skills from "../../components/Skills";
 
 const SkillsPage: NextPage = () => {
   return <Skills />;
